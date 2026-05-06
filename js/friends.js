9b7c5ff94161b5b1dcf5a5331473342d9e8a38b7
bodealarif-cell <bodealarif@gmail.com>
2026-05-06 22:29:32 +0300

    Update friends.js

--- js/friends.js
+++ js/friends.js
@@ -1,5 +1,5 @@
 import { db } from './firebase.js';
-import { collection, query, where, getDocs, addDoc, updateDoc, doc, arrayUnion, writeBatch, serverTimestamp, onSnapshot } from 'firebase/firestore';
+import { collection, query, where, getDocs, addDoc, updateDoc, doc, arrayUnion, writeBatch, serverTimestamp, onSnapshot } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 import { showToast, showLoading, escapeHTML } from './ui.js';
 
 let currentUser = null;
